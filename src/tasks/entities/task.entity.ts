0ef373cec9a891aa11afe2db2da1a3f72b96a33d
Andrés Cárdenas <mandrescardenash@gmail.com>
2025-05-23 19:23:11 -0500

    feat: implement task entity, DTOs, and service methods for task management

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,8 @@
-export class Task {}
+export class Task {
+	id: number;
+	title: string;
+	description: string;
+	status: string = 'created';
+	created_at: Date;
+	updated_at: Date;
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,26 +1,147 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
 
+type responseData = {
+	error: boolean;
+	msg: string;
+	data: Task | null;
+};
+
+type responseDataArr = {
+	error: boolean;
+	msg: string;
+	data: Task[] | null;
+};
+
+const listTaks = [
+	{
+		id: 1,
+		title: 'task one',
+		description: 'desct one',
+		status: 'created',
+		created_at: new Date(),
+		updated_at: new Date(),
+	},
+];
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
-  }
+	create(createTaskDto: CreateTaskDto) {
+		const dta: Task = {
+			id: Math.floor(Math.random() * 100),
+			title: createTaskDto.title,
+			description: createTaskDto.description,
+			status: 'created',
+			created_at: new Date(),
+			updated_at: new Date(),
+		};
+		listTaks.push(dta);
+		return dta;
+	}
 
-  findAll() {
-    return `This action returns all tasks`;
-  }
+	findAll(status?: string): responseDataArr {
+		switch (status) {
+			case 'created':
+				return {
+					error: false,
+					msg: 'Taks',
+					data: listTaks.filter((task) => task.status == 'created'),
+				};
+			case 'doing':
+				return {
+					error: false,
+					msg: 'Taks',
+					data: listTaks.filter((task) => task.status == 'doing'),
+				};
+			case 'deleted':
+				return {
+					error: false,
+					msg: 'Taks',
+					data: listTaks.filter((task) => task.status == 'deleted'),
+				};
+			case 'completed':
+				return {
+					error: false,
+					msg: 'Taks',
+					data: listTaks.filter((task) => task.status == 'completed'),
+				};
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
-  }
+			default:
+				return {
+					error: false,
+					msg: 'Taks',
+					data: listTaks.filter((task) => task.status != 'deleted'),
+				};
+		}
+	}
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
-  }
+	findOne(id: number): responseData {
+		const findTask = listTaks.find((task) => task.id === id);
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
-  }
+		if (!findTask) {
+			return {
+				error: true,
+				msg: 'Task not found',
+				data: null,
+			};
+		}
+
+		return {
+			error: false,
+			msg: 'Task find',
+			data: findTask,
+		};
+	}
+
+	update(id: number, updateTaskDto: UpdateTaskDto): responseData {
+		const findTask = listTaks.find((task) => task.id === id);
+
+		if (!findTask) {
+			return {
+				error: true,
+				msg: 'Task not found',
+				data: null,
+			};
+		}
+
+		if (updateTaskDto.title !== undefined) {
+			findTask.title = updateTaskDto.title;
+		}
+		if (updateTaskDto.description !== undefined) {
+			findTask.description = updateTaskDto.description;
+		}
+		if (updateTaskDto.status !== undefined) {
+			findTask.status = updateTaskDto.status;
+		}
+
+		findTask.updated_at = new Date();
+
+		return {
+			error: false,
+			msg: 'Task updated',
+			data: findTask,
+		};
+	}
+
+	remove(id: number) {
+		const findTask = listTaks.find((task) => task.id === id);
+
+		if (!findTask) {
+			return {
+				error: true,
+				msg: 'Task not found',
+				data: null,
+			};
+		}
+
+		// delete
+		findTask.status = 'deleted';
+
+		return {
+			error: false,
+			msg: 'Task find',
+			data: findTask,
+		};
+	}
 }
